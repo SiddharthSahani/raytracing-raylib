cf89afb28cb97c9156882a4f8011373f565cfc5d
Siddharth Sahani <sidhucroedde@gmail.com>
2024-05-23 19:25:23 +0530

    Shading with normals

--- shaders/raytracing.glsl
+++ shaders/raytracing.glsl
@@ -1,6 +1,8 @@
 
 #version 330 core
 
+#define FLT_MAX 3.402823466e+38F
+
 
 uniform vec2 uImageSize;
 uniform mat4 uInvViewMat;
@@ -20,6 +22,13 @@ struct Sphere {
 };
 
 
+struct HitRecord {
+    vec3 worldPosition;
+    vec3 worldNormal;
+    float hitDistance;
+};
+
+
 Ray genRay() {
     vec2 coord = gl_FragCoord.xy / uImageSize * 2.0 - 1.0;
     vec4 target = uInvProjMat * vec4(coord, 1.0, 1.0);
@@ -29,24 +38,39 @@ Ray genRay() {
 }
 
 
-bool hitSphere(Sphere sphere, Ray ray) {
+void hitSphere(Sphere sphere, Ray ray, out HitRecord record) {
     vec3 oc = ray.origin - sphere.position;
     float a = dot(ray.direction, ray.direction);
     float b = 2.0 * dot(oc, ray.direction);
     float c = dot(oc, oc) - sphere.radius * sphere.radius;
     float d = b * b - 4 * a * c;
-    return d > 0.0;
+
+    if (d < 0.0f) {
+        return;
+    }
+
+    float t = (-b - sqrt(d)) / (2.0 * a);
+
+    if (t < record.hitDistance) {
+        record.worldPosition = ray.origin + ray.direction * t;
+        record.worldNormal = normalize(record.worldPosition - sphere.position);
+        record.hitDistance = t;
+    }
 }
 
 
 void main() {
     Ray ray = genRay();
-    
     Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 1.0);
 
-    if (hitSphere(sphere, ray)) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    HitRecord record;
+    record.hitDistance = FLT_MAX;
+
+    hitSphere(sphere, ray, record);
+
+    if (record.hitDistance == FLT_MAX) {
+        gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
     } else {
-        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+        gl_FragColor = vec4(record.worldNormal, 1.0);
     }
 }
