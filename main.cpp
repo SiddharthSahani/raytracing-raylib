b85c0f8b6b06c5053c8d57f38a5247a0393d89dd
Siddharth Sahani <sidhucroedde@gmail.com>
2024-07-08 13:09:27 +0530

    Random scene generation

--- main.cpp
+++ main.cpp
@@ -3,39 +3,48 @@
 #include "src/renderer.h"
 
 
-// rt::Scene createRandomScene(int numSpheres) {
-//     SetRandomSeed(0);
+rt::CompiledScene createRandomScene(int numSpheres, int numMats) {
+    SetRandomSeed(0);
 
-//     rt::Scene scene;
+    rt::Scene scene;
 
-//     for (int i = 0; i < 5; i++) {
-//         Color col = {
-//             GetRandomValue(0, 255),
-//             GetRandomValue(0, 255),
-//             GetRandomValue(0, 255),
-//             255,
-//         };
-//         float roughness = i / 4.0;
+    std::vector<std::shared_ptr<rt::Material>> materials;
+    for (int i = 0; i < numMats; i++) {
+        Vector4 col = ColorNormalize({
+            .r = (uint8_t) GetRandomValue(0, 255),
+            .g = (uint8_t) GetRandomValue(0, 255),
+            .b = (uint8_t) GetRandomValue(0, 255),
+            .a = 255,
+        });
+        float roughness = GetRandomValue(0, 5) / 10.0;
 
-//         rt::Material material = rt::Material(col, roughness, 0.0);
-//         scene.setMaterial(material);
-//     }
+        auto mat = std::make_shared<rt::Material>();
+        mat->setAlbedo({.value = {col.x, col.y, col.z}, .deviation = 0.05});
+        mat->setRoughness({.value = roughness, .deviation = 0.01});
+        materials.push_back(mat);
+    }
 
-//     for (int i = 0; i < numSpheres; i++) {
-//         Vector3 pos = {
-//             GetRandomValue(-40000, 40000) / 10000.0f,
-//             GetRandomValue(-40000, 40000) / 10000.0f,
-//             GetRandomValue(-40000, 40000) / 10000.0f,
-//         };
-//         float rad = GetRandomValue(5000, 10000) / 10000.0f;
+    for (int i = 0; i < numSpheres; i++) {
+        Vector3 pos = {
+            GetRandomValue(-40000, 40000) / 10000.0f,
+            GetRandomValue(-40000, 40000) / 10000.0f,
+            GetRandomValue(-40000, 40000) / 10000.0f,
+        };
+        float rad = GetRandomValue(5000, 10000) / 10000.0f;
+        auto mat = materials[GetRandomValue(0, numMats-1)];
 
-//         rt::Sphere sph = rt::Sphere{pos, rad, GetRandomValue(0, 4)};
-//         scene.addObject(sph);
-//     }
+        rt::Sphere sph = {
+            .position = pos,
+            .radius = rad,
+            .material = mat,
+        };
+        scene.addObject(sph);
+    }
 
-//     scene.backgroundColor = {210, 210, 240, 255};
-//     return scene;
-// }
+    scene.backgroundColor = {210, 210, 240, 255};
+
+    return rt::CompiledScene(scene, {2048, 2048});
+}
 
 
 rt::CompiledScene createScene_1() {
@@ -137,6 +146,14 @@ rt::CompiledScene createScene_1() {
 // }
 
 
+bool changeIndex(unsigned int& index, KeyboardKey key) {
+    unsigned int tmp = index;
+    index -= (IsKeyDown(key) && IsKeyPressed(KEY_LEFT));
+    index += (IsKeyDown(key) && IsKeyPressed(KEY_RIGHT));
+    return index != tmp;
+}
+
+
 int main() {
     const int windowWidth = 1280;
     const int windowHeight = 720;
@@ -169,8 +186,8 @@ int main() {
     const rt::CompiledScene scenes[] = {
         createScene_1(),
         // createScene_2(),
-        // createRandomScene(8),
-        // createRandomScene(16),
+        createRandomScene(8, 4),
+        createRandomScene(16, 4),
     };
     const int numScenes = sizeof(scenes) / sizeof(rt::CompiledScene);
 
@@ -203,23 +220,9 @@ int main() {
             }
         }
 
-        if (IsKeyDown(KEY_C)) {
-            if (IsKeyPressed(KEY_LEFT)) {
-                configIdx -= 1;
-            }
-            if (IsKeyPressed(KEY_RIGHT)) {
-                configIdx += 1;
-            }
-        }
-        if (IsKeyDown(KEY_S)) {
-            if (IsKeyPressed(KEY_LEFT)) {
-                sceneIdx -= 1;
-                renderer.resetImage();
-            }
-            if (IsKeyPressed(KEY_RIGHT)) {
-                sceneIdx += 1;
-                renderer.resetImage();
-            }
+        changeIndex(configIdx, KEY_C);
+        if (changeIndex(sceneIdx, KEY_S)) {
+            renderer.resetImage();
         }
 
         const rt::CompiledScene& scene = scenes[sceneIdx % numScenes];
